07932bcc79c9eac5911388d9350fc2abebaaa13e
Robert Vitoriano <robertvitoriano@gmail.com>
2021-11-02 11:32:31 -0300

    feature:encrypt passwords

--- src/controllers/SupervisorController.ts
+++ src/controllers/SupervisorController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import {User} from '../entities/User';
 import { getRepository, Repository} from 'typeorm'
+import bcrypt from   'bcryptjs';
 
 export default class SupervisorController {
 
@@ -15,20 +16,28 @@ export default class SupervisorController {
   public  getSupervisorById = async(req: Request, res: Response) => {}
 
   public  createSupervisor = async(req: Request, res: Response) => {
+    try{
 
-    this.repository = getRepository(User)
+      this.repository = getRepository(User)
 
-    const supervisor = await this.repository.create({
-      email: 'email@teste.com',
-      password: '123156',
-      name: 'naaam',
-      level:'supervisor'
-    })
-
-    this.repository.save(supervisor)
-
-    res.json(supervisor);
-
+      const{email, password, name, level} = req.body
+  
+      const encryptedPassword = await bcrypt.hash(String(password), 8)
+  
+      const supervisor = await this.repository.create({
+        email,
+        password:String(encryptedPassword),
+        name,
+        level:'supervisor'
+      })
+  
+      this.repository.save(supervisor)
+  
+      res.json(supervisor);
+      
+    }catch(error){
+      console.log(error);
+    }
   }
 
 }
