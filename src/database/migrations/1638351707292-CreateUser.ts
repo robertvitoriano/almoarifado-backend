2b4c219904c19b7fe955580c5e74e4281668ad77
Robert Vitoriano <robertvitoriano@gmail.com>
2021-12-01 06:57:16 -0300

    add migrations

--- src/database/migrations/1638351707292-CreateUser.ts
+++ src/database/migrations/1638351707292-CreateUser.ts
@@ -1,11 +1,44 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
-
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 export class CreateUser1638351707292 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:'users',
+                columns:[
+                    {
+                        name:'id',
+                        type:'varchar',
+                    },
+                    {
+                        name:'name',
+                        type:'varchar',
+                    },
+                    {
+                        name:'password',
+                        type:'varchar',
+                    },
+                    {
+                        name:'email',
+                        type:'varchar',
+                    },
+                    {
+                        name:'level',
+                        type:'varchar',
+                    },
+                    {
+                        name:'created_at',
+                        type:'timestamp',
+                        default:'now()',
+                    }
+                ]
+
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
     }
 
 }
